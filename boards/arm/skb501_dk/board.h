c77eae381835750538769283a4a44976b25eca66
Jesper Derander <jesper.derander@endian.se>
2018-11-19 15:16:31 +0100

    Added missing board.h file from last commit

---
+++ boards/arm/skb501_dk/board.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2016-2018 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* Onboard GREEN LED 0 */
+#define LED0_GPIO_PIN  6
+#define LED0_GPIO_PORT CONFIG_GPIO_P0_DEV_NAME
+
+#endif /* __INC_BOARD_H */
